2b627478d3ce472390407b398d1a53644f335a14
emilychen <yenjungc@smu.edu>
2022-09-13 21:16:29 +0800

    fix: [LOT-5] use modal as common dialog template

--- components/Dialog/DeleteDialog/DeleteDialog.jsx
+++ components/Dialog/DeleteDialog/DeleteDialog.jsx
@@ -1,11 +1,8 @@
-import styles from '../Dialog.module.css';
 import Spacer from '../../../components/Layout/Spacer';
 import { useTranslation } from 'react-i18next';
+import { Modal } from '../../Modal';
 
 const classes = {
-  modalContainer: "flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none",
-  modalOuterLayout: "relative p-6 w-full max-w-2xl md:h-auto",
-  modalInnerLayout: "border-0 rounded-lg shadow-lg relative flex flex-col w-full h-full bg-white outline-none focus:outline-none",
   modalRowContainer: "flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b",
   modalClose: "text-blue-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1",
   modalSave: "text-white bg-blue-500 active:bg-blue-700 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
@@ -16,36 +13,25 @@ const DeleteDialog = (props) => {
   const { onClose, onDelete, rowData } = props;
 
   return (<>
-    <div className={classes.modalContainer}>
-      <div className={classes.modalOuterLayout}>
-        <div className={classes.modalInnerLayout}>
-          <div className={classes.modalClose}>
-            <button className="bg-transparent border-0 text-black float-right"
+      <Modal
+        onClose={onClose}>
+        <form
+          onSubmit={(e) => onDelete(e, rowData)}
+          className="shadow-md rounded px-8 pt-6 pb-8 w-full">
+          <div>{t('MESSAGE.DELETE')}</div>
+          <Spacer size={0.5} axis="vertical" />
+          <div className={classes.modalRowContainer}>
+            <button className={classes.modalClose}
+              type="button"
               onClick={onClose}
-              ><span className={styles.close}>x</span>
-            </button>
+              >{t('COMMON.NO')}</button>
+            <button
+              type="submit"
+              className={classes.modalSave}
+              >{t('COMMON.YES')}</button>
           </div>
-          <div className="relative p-6 flex-auto text-left">
-              <form
-                onSubmit={(e) => onDelete(e, rowData)}
-                className="shadow-md rounded px-8 pt-6 pb-8 w-full">
-                <div>{t('MESSAGE.DELETE')}</div>
-                <Spacer size={0.5} axis="vertical" />
-                <div className={classes.modalRowContainer}>
-                  <button className={classes.modalClose}
-                    type="button"
-                    onClick={onClose}
-                    >NO</button>
-                  <button
-                    type="submit"
-                    className={classes.modalSave}
-                    >YES</button>
-                </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
+        </form>
+      </Modal>
   </>
   );
 };
